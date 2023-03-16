a1448b666fa380df58712229f2cea01bd0ba365c
rn-mgl <reinb2ra@gmail.com>
2023-03-16 08:16:44 +0800

    ui

--- app/auth/verify/page.tsx
+++ app/auth/verify/page.tsx
@@ -34,7 +34,7 @@ export default function verify() {
 
   return (
     <>
-      <div className="min-h-screen cstm-flex-col p-5 gap-5">
+      <div className="cstm-grdbg-blk-1-2 h-auto min-h-screen cstm-flex-col p-5 gap-5">
         <div className="cstm-flex-col gap-2">
           <p
             className="font-noto text-wht font-bold text-center animate-dropOpacity
